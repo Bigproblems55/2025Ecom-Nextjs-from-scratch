a7f5566a455c26093628b6b7f9c9a356857a0cf9
Nicholas Lacapria <jack.problems@protonmail.com>
2026-09-12 03:56:55 -0700

    moving glbals.css around

---
+++ prostore/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({subsets: ['latin']});
+
+export const metadata: Metadata = {
+  title: "Online Extrusion",
+  description: "A mondern ecommerce platform built for shopping with Next.js",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en" 
+    >
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
