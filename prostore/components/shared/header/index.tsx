40c12e8f0eea7c7d9024d03890d013ad0d40516d
Nicholas Lacapria <jack.problems@protonmail.com>
2026-09-13 04:54:12 -0700

    Added a cart button with a link to the shoppingCart element though it has not been made. For somereason asChild for Button will not work.

--- prostore/components/shared/header/index.tsx
+++ prostore/components/shared/header/index.tsx
@@ -12,12 +12,19 @@ const Header = () => {
                     <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} 
                         height={48} 
                         width={48} 
-                        priority={true}
+                        priority={true} // Because main image and warning if you don't
                         />
                         <span className="hidden lg:block font-bold text-2xl ml-3">
                             {APP_NAME}
                         </span>
                 </Link>
+            </div>
+            <div className='space-x-2'>
+                <Button asChild variant='ghost'>
+                    <Link href='/cart'>
+                        <ShoppingCart />
+                    </Link>
+                </Button>
             </div>
         </div>
     </header>;
