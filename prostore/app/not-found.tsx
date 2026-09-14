f0f7d9998239a378260468ae319798a40419235e
Nicholas Lacapria <jack.problems@protonmail.com>
2026-09-14 06:19:51 -0700

    Adding page not found page with image and home page button on it

--- prostore/app/not-found.tsx
+++ prostore/app/not-found.tsx
@@ -4,10 +4,20 @@ import {APP_NAME} from '@/lib/constants';
 import Image from 'next/image';
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Image src="/images/logo.svg" width={48} height={48} alt={`${APP_NAME} logo`} priority={true}/>
-      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-lg text-gray-600">Sorry, the page you are looking for does not exist.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image src="/images/logo.svg" width={48} height={48} 
+      alt={`${APP_NAME} logo`} priority={true}/>
+      <div p-6  w-1-3 rounded-lg shadow-md text-center>
+        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+        <p className="text-destructive">Could Not find requested resource</p>
+        <Button nativeButton={false} onClick={() => (window.location.href = '/')} 
+        className="mt-4 ml-2">
+          Back to Home
+        </Button>
+      </div>
+      
+      {/* <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="text-lg text-gray-600">Sorry, the page you are looking for does not exist.</p> */}
     </div>
   );
 };
