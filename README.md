00e68734bf8161d5e21c14e455bdd1c4bbff3fdb
Nicholas Lacapria <jack.problems@protonmail.com>
2026-09-14 18:54:46 -0700

    Adding sample data for sample products with a limit of 4 on the homepage

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Teachers Edition:
 git@github.com:bradtraversy/prostore.git
 Teachers Demo: 
 https://prostore-one.verel.app/z/h 
+sfc is a fast way to get a component started in typscript easy with visual studio intellesense.
--- prostore/app/(root)/page.tsx
+++ prostore/app/(root)/page.tsx
@@ -1,4 +1,5 @@
-
+import sampleData from '@/db/sample-data'
+import ProductList from '@/components/shared/product/product-list'
 export const metadata = {
   // This will change the page title
   title: 'Home',
@@ -6,8 +7,16 @@ export const metadata = {
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Homepage = async () =>{
-  await delay(2000);
-  return <>Online Extrusion</>
-};
+  await delay(800);
+  console.log(sampleData);
+  return <>
+    <ProductList 
+    data={sampleData.products} 
+    title="Newest Arrivals"
+    limit={4}
+      />
+    
+  </>
+  };
 
 export default Homepage;
