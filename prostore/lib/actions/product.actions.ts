7e6ac1cbb170abb7e17f0ccbf542c16c728428a6
Nicholas Lacapria <jack.problems@protonmail.com>
2026-09-16 03:02:48 -0700

    Adding product description with the product slug name being found

--- prostore/lib/actions/product.actions.ts
+++ prostore/lib/actions/product.actions.ts
@@ -1,19 +1,10 @@
 'use server';
 import { prisma } from '@/db/prisma';
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { PrismaClient } from "@/lib/generated/prisma/client";
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get latest products
 export async function getLatestProducts() {
-
-// TAken out due to prisma.ts
-    //     const prisma = new PrismaClient({
-//     adapter: new PrismaPg({
-//         connectionString: process.env.DATABASE_URL!,
-//     }),
-// });
 
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
@@ -24,4 +15,11 @@ export async function getLatestProducts() {
 
 
     return convertToPlainObject(data);
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string){
+    return await prisma.product.findFirst({
+        where: {slug:slug},
+    })
 }
