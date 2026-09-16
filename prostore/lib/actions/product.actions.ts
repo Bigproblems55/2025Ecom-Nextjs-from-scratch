2fad5f61d9d0f5dc00cc35890fc1195f8ba14aa5
Nicholas Lacapria <jack.problems@protonmail.com>
2026-09-16 02:35:10 -0700

    Added Serverless Environment Config file so prisma is ran from the prisma.ts file instead of using client and the prisma it comes with. This makes it headless having this serverless config file handy

--- prostore/lib/actions/product.actions.ts
+++ prostore/lib/actions/product.actions.ts
@@ -1,17 +1,19 @@
 'use server';
-
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/lib/generated/prisma/client";
+import { prisma } from '@/db/prisma';
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@/lib/generated/prisma/client";
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
 // Get latest products
 export async function getLatestProducts() {
-    const prisma = new PrismaClient({
-    adapter: new PrismaPg({
-        connectionString: process.env.DATABASE_URL!,
-    }),
-});
+
+// TAken out due to prisma.ts
+    //     const prisma = new PrismaClient({
+//     adapter: new PrismaPg({
+//         connectionString: process.env.DATABASE_URL!,
+//     }),
+// });
 
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
