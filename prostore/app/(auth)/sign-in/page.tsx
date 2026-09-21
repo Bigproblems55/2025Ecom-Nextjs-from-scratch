097a297867b50ad864dcf707fa52c8867a577565
Nicholas Lacapria <jack.problems@protonmail.com>
2026-09-21 16:38:27 -0700

    Made signup page with Links thatwill go to pages related to what link or button was clicked on to sign in or sign-up

--- prostore/app/(auth)/sign-in/page.tsx
+++ prostore/app/(auth)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image";
 import { APP_NAME }from '@/lib/constants';
-
+import CredentialsSignInForm from "./credentials-signin-form";
 
 
 export const metadata: Metadata = {
@@ -28,7 +28,7 @@ const SignInPage = () => {
                 </CardDescription>
             </CardHeader>
             <CardContent className="space-y-4">
-                {/* FORM HERE */}
+                {<CredentialsSignInForm />}
             </CardContent>
         </Card>
 
--- prostore/lib/constants/index.ts
+++ prostore/lib/constants/index.ts
@@ -2,3 +2,7 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME ||  'Online Extrusion'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Online Extrusion is a all wrist watch shopping website";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 6;
+export const signInDefaultValues  = {
+    email:'',
+    password:''
+};
